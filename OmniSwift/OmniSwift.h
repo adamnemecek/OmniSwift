613b4cd72203c38597da25006a85e29e21beee70
CooperCorona <cooperk@plvwtelco.net>
2015-05-27 16:19:58 -0500

    Initial Commit

---
+++ OmniSwift/OmniSwift.h
@@ -0,0 +1,19 @@
+//
+//  OmniSwift.h
+//  OmniSwift
+//
+//  Created by Cooper Knaak on 5/27/15.
+//  Copyright (c) 2015 Cooper Knaak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OmniSwift.
+FOUNDATION_EXPORT double OmniSwiftVersionNumber;
+
+//! Project version string for OmniSwift.
+FOUNDATION_EXPORT const unsigned char OmniSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OmniSwift/PublicHeader.h>
+
+
